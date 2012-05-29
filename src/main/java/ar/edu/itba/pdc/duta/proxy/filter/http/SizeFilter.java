4febefc1e7126b30c2971abdf3eaaf36ff34f7be
Juan Pablo Civile <elementohb@gmail.com>
2012-05-29 02:33:19 -0300

    Add the file size filter (Close #13)

--- src/main/java/ar/edu/itba/pdc/duta/proxy/filter/http/SizeFilter.java
+++ src/main/java/ar/edu/itba/pdc/duta/proxy/filter/http/SizeFilter.java
@@ -14,39 +14,82 @@ import ar.edu.itba.pdc.duta.proxy.filter.FilterPart;
 import ar.edu.itba.pdc.duta.proxy.filter.Interest;
 import ar.edu.itba.pdc.duta.proxy.operation.Operation;
 
-public class BlockFilter implements Filter {
+public class SizeFilter implements Filter {
 	
 	{
-		Stats.registerFilterType(BlockFilter.class);
+		Stats.registerFilterType(SizeFilter.class);
+	}
+
+
+	private final int maxSize;
+	
+	public SizeFilter(int maxSize) {
+		super();
+		this.maxSize = maxSize;
 	}
 
 	@Override
 	public FilterPart getRequestPart() {
-		return new RequestPart();
+		return null;
 	}
 
 	@Override
 	public FilterPart getResponsePart() {
-		return null;
+		return new ResponsePart();
 	}
-
-	public static class RequestPart extends FilterPart {
+	
+	public class ResponsePart extends FilterPart {
 		
+		private Integer size = null;
+
 		@Override
 		public Interest checkInterest(MessageHeader header) {
-			return new Interest(true, false, false);
+			
+			boolean isSizeKnow = false;
+			String field = header.getField("Content-Length");
+			if (field != null) {
+				
+				try {
+					size = Integer.valueOf(field.trim());
+					isSizeKnow = true;
+				} catch (NumberFormatException e) {
+					// No biggie!
+				}
+				
+			}
+			
+			return new Interest(true, !isSizeKnow, false);
+		}
+		@Override
+		public Message processHeader(Operation op, MessageHeader header) {
+			
+			if (size != null && size >= maxSize) {
+				return block();
+			}
+			
+			return null;
 		}
 		
 		@Override
-		public Message processHeader(Operation op, MessageHeader header) {
+		public Message bytesRecieved(Operation op, Message msg, long delta, long total) {
+			
+			if (total >= maxSize) {
+				return block();
+			}
+			
+			return null;
+		}
+		
+		private Message block() {
 			Map<String, String> fields = new HashMap<String, String>();
 			
 			fields.put("Date", new Date().toString());
-			fields.put("Content-Type", "text/html");
 			fields.put("Content-Length", "0");
 			
 			ResponseHeader headers = new ResponseHeader(Grammar.HTTP11, 404, "Not Found", fields);
 			return new Message(headers);
-		}		
+		}
+		
 	}
+
 }
