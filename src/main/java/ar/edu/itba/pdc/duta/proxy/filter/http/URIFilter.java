13e11f351d42835fa7b4c04c9fefc6ba41172ca8
Dario Sneidermanis <dariosn@gmail.com>
2012-06-04 05:50:59 -0300

    uri filter

--- src/main/java/ar/edu/itba/pdc/duta/proxy/filter/http/URIFilter.java
+++ src/main/java/ar/edu/itba/pdc/duta/proxy/filter/http/URIFilter.java
@@ -8,6 +8,7 @@ import java.util.regex.PatternSyntaxException;
 
 import ar.edu.itba.pdc.duta.admin.Stats;
 import ar.edu.itba.pdc.duta.http.Grammar;
+import ar.edu.itba.pdc.duta.http.MessageFactory;
 import ar.edu.itba.pdc.duta.http.model.Message;
 import ar.edu.itba.pdc.duta.http.model.MessageHeader;
 import ar.edu.itba.pdc.duta.http.model.RequestHeader;
@@ -18,34 +19,28 @@ import ar.edu.itba.pdc.duta.proxy.filter.Interest;
 import ar.edu.itba.pdc.duta.proxy.operation.Operation;
 
 public class URIFilter implements Filter {
-	
+
 	{
 		Stats.registerFilterType(URIFilter.class);
 	}
 
 	private final Pattern pattern; 
-	private boolean blocked=false;
-	
+
 	public URIFilter(String uriBlocked) {
-		super();
-		Pattern p;
-		try{
-			p = Pattern.compile(uriBlocked);
-		} catch (PatternSyntaxException pE) {
-			//invalid regular expression
-			p = null;
-		}
-		this.pattern = p;
+
+		pattern = Pattern.compile(uriBlocked);
 	}
 
 	@Override
 	public FilterPart getRequestPart() {
+
 		return new RequestPart();
 	}
 
 	@Override
 	public FilterPart getResponsePart() {
-		return new  ResponsePart();
+
+		return null;
 	}
 	
 	private class RequestPart extends FilterPart {
@@ -57,49 +52,16 @@ public class URIFilter implements Filter {
 
 		@Override
 		public Message processHeader(Operation op, MessageHeader header) {
-			
-			if(pattern == null) {
-				return null;
-			}
-			
-			RequestHeader request = (RequestHeader) header;
-			
-			String url = header.getField("host") + request.getRequestURI();
-			
-			if(pattern.matcher(url).find()){
-				blocked = true;
-			}
-			
-			return null;
-		}
 
-	}
-	
-	private  class ResponsePart extends FilterPart {
-		
-		@Override
-		public Interest checkInterest(MessageHeader header) {
-			return new Interest(true, false, false);
-		}
-		
-		@Override
-		public Message processHeader(Operation op, MessageHeader header) {
-			if(blocked) {
-				return block();
+			String url = op.getServerProxy().getAddress().getHostName() + ((RequestHeader) header).getRequestURI();
+
+			if (pattern.matcher(url).matches()) {
+
+				return MessageFactory.build404();
 			}
+
 			return null;
 		}
-		
-		private Message block() {
-			Map<String, String> fields = new HashMap<String, String>();
-			
-			fields.put("Date", new Date().toString());
-			fields.put("Content-Length", "0");
-			
-			ResponseHeader headers = new ResponseHeader(Grammar.HTTP11, 404, "Not Found", fields);
-			return new Message(headers);
-		}
-		
 	}
 
 	@Override
