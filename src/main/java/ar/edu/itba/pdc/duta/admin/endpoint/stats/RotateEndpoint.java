c2fd63dc6f398e809b77baa605cf4535b003ed0e
Juan Pablo Civile <elementohb@gmail.com>
2012-06-04 13:08:26 -0300

    Show the stats for the rotate end point properly

--- src/main/java/ar/edu/itba/pdc/duta/admin/endpoint/stats/RotateEndpoint.java
+++ src/main/java/ar/edu/itba/pdc/duta/admin/endpoint/stats/RotateEndpoint.java
@@ -1,12 +1,14 @@
 package ar.edu.itba.pdc.duta.admin.endpoint.stats;
 
+import ar.edu.itba.pdc.duta.admin.Stats;
+import ar.edu.itba.pdc.duta.proxy.filter.http.ImageRotationFilter;
+
 
 public class RotateEndpoint extends AbstractStatsEndpoint {
 
 	@Override
 	protected long getValue() {
-		return 0;
-		//return Stats.filterMatches(IDontExist.class);
+		return Stats.filterMatches(ImageRotationFilter.class);
 	}
 
 }
