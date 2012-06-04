b925eec68a07673b97e151c6ea19ad5465af46a0
Dario Sneidermanis <dariosn@gmail.com>
2012-06-04 07:27:15 -0300

    image rotation filter.. has to be testedgit status

---
+++ src/main/java/ar/edu/itba/pdc/duta/proxy/filter/http/ImageRotationFilter.java
@@ -0,0 +1,122 @@
+package ar.edu.itba.pdc.duta.proxy.filter.http;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageInputStream;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+
+import ar.edu.itba.pdc.duta.admin.Stats;
+import ar.edu.itba.pdc.duta.http.MessageFactory;
+import ar.edu.itba.pdc.duta.http.model.Message;
+import ar.edu.itba.pdc.duta.http.model.MessageHeader;
+import ar.edu.itba.pdc.duta.net.buffer.DataBuffer;
+import ar.edu.itba.pdc.duta.proxy.filter.Filter;
+import ar.edu.itba.pdc.duta.proxy.filter.FilterPart;
+import ar.edu.itba.pdc.duta.proxy.filter.Interest;
+import ar.edu.itba.pdc.duta.proxy.operation.Operation;
+
+public class ImageRotationFilter implements Filter {
+
+	{
+		Stats.registerFilterType(ImageRotationFilter.class);
+	}
+
+	@Override
+	public FilterPart getRequestPart() {
+
+		return null;
+	}
+
+	@Override
+	public FilterPart getResponsePart() {
+
+		return new ResponsePart();
+	}
+
+	private static class ResponsePart extends FilterPart {
+
+		private Logger logger = Logger.getLogger(ResponsePart.class);
+
+		private MediaType contentType;
+
+		@Override
+		public Interest checkInterest(MessageHeader header) {
+
+			boolean isImage = false;
+
+			contentType = MediaType.valueOf(header.getField("Content-Type")); 
+
+			if (contentType.isCompatible(MediaType.valueOf("image/*"))) {
+
+				isImage = true;
+			}
+
+			return new Interest(false, false, isImage);
+		}
+
+		@Override
+		public Message filter(Operation op, Message msg) {
+
+			byte[] bytes = new byte[msg.getCurrentBodySize()];
+
+			try {
+
+				msg.getBody().get(bytes, 0, msg.getCurrentBodySize());
+
+		        Iterator<ImageReader> readers = ImageIO.getImageReadersByMIMEType(contentType.toString());
+		        ImageReader reader = readers.next();
+		        reader.setInput(ImageIO.createImageInputStream(new ByteArrayInputStream(bytes)));
+		        BufferedImage image = reader.read(0);
+
+		        AffineTransform tx = AffineTransform.getScaleInstance(-1, -1);
+		        tx.translate(-image.getWidth(null), -image.getHeight(null));
+		        AffineTransformOp txop = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+				image = txop.filter(image, null);
+
+				Iterator<ImageWriter> writers = ImageIO.getImageWritersByMIMEType(contentType.toString());
+				ImageWriter writer = writers.next();
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				writer.setOutput(ImageIO.createImageOutputStream(baos));
+				writer.write(image);
+
+				baos.flush();
+				bytes = baos.toByteArray();
+				baos.close();
+
+				msg.setBody(new DataBuffer(bytes));
+
+			} catch (IOException e) {
+
+				logger.error("Failed to read/write image", e);
+				return MessageFactory.build500();
+
+			}  catch (NoSuchElementException e) {
+
+				logger.error("Invalid image type", e);
+				return MessageFactory.build500();
+			}
+
+			return null;
+		}
+	}
+
+	@Override
+	public int getPriority() {
+
+		return 0;
+	}
+}
